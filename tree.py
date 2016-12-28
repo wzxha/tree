245120d03183c7698b39c8d7a3dca60f82fbdf17
wzxjiang <wzxjiang@foxmail.com>
2016-12-28 14:40:41 +0800

    You can control the number of twigs

--- tree.py
+++ tree.py
@@ -2,8 +2,9 @@
 import sys, os
 
 class Tree():
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, path, scale = 0):
+        self.path  = path
+        self.scale = int(scale)
 
     def show_tree(self):
         complete_list = (self.get_file_list(self.path, self.path.split('/')[-1]))
@@ -15,12 +16,16 @@ class Tree():
             return list
 
         for sub_path in os.listdir(path):
-            if sub_path == '.DS_Store':
+            if sub_path in ['.DS_Store' , '.svn']:
                 continue
             list.append(self.get_file_list(path + '/' + sub_path, sub_path))
+
         return list
 
     def show_brance(self, arr, scale=0, groups = [0],  end=False):
+        if self.scale > 0 and scale > self.scale:
+            return
+
         space = '|'
         if scale == 0:
             space = '├'
@@ -48,16 +53,19 @@ class Tree():
                      self.show_brance(x, scale+1, groups, True)
                 else:
                     self.show_brance(x, scale+1, groups, False)
+
             elif isinstance(x, str):
                 print   space + '── ' + x
 
 
-
 if __name__ == '__main__':
-    path = os.getcwd()
+    path  = os.getcwd()
+    scale = 0
 
     if (len(sys.argv) > 1):
         path = sys.argv[1]
+    if (len(sys.argv) > 2):
+        scale = sys.argv[2]
 
-    tree = Tree(path)
+    tree = Tree(path, scale)
     tree.show_tree()
